c802ed44fee97166c5de5c32c530a884526a8664
suanhwee <suanhwee@gmail.com>
2021-04-05 08:01:40 +0800

    search

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import React , {Component} from 'react'
+import React, { Component } from "react";
 
-import './App.css';
-import { within } from '@testing-library/dom';
-import { CardList } from './components/card-list/card-list.component';
+import "./App.css";
+import { within } from "@testing-library/dom";
+import { CardList } from "./components/card-list/card-list.component";
 
-class App extends Component{
-  constructor(){
+class App extends Component {
+  constructor() {
     super();
-    this.state={
-      monsters:[]
+    this.state = {
+      monsters: [],
     };
-
   }
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response=>response.json())
-    .then(users=>this.setState({monsters:users}))
-  
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
-  render(){
-  return (
-    <div className="App">
-      <CardList monsters={this.state.monsters}>
-        
-      
-      </CardList>
-     
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={(e) => {
+            this.setState({ searchField: e.target.value });
+          }}
+        />
+        <CardList monsters={this.state.monsters}></CardList>
+      </div>
+    );
   }
 }
 
